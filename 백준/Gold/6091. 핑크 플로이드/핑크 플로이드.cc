2a899cb98a7752770201d433cc813452ec86a994
선민영 <109517191+stellaMY@users.noreply.github.com>
2023-04-14 16:36:40 +0900

    [Gold I] Title: 핑크 플로이드, Time: 488 ms, Memory: 33448 KB -BaekjoonHub

--- 백준/Gold/6091. 핑크 플로이드/핑크 플로이드.cc
+++ 백준/Gold/6091. 핑크 플로이드/핑크 플로이드.cc
@@ -2,28 +2,10 @@
 #include <vector>
 using namespace std;
 
-/*
-int INF = 1000000;
-
-    int a[4][4] = {
-      { 0, 5, INF, 8 },
-      { 7, 0, 9, INF },
-      { 2, INF, 0, 4 },
-      { INF, INF, 3, 0 }
-    };
-
-    // 시간복잡도 V^3
-    for (int k = 0; k < 4; k++)  // k 는 거쳐가는 정점
-        for (int i = 0; i < 4; i++)  // i 는 행 (출발 정점)
-            for (int j = 0; j < 4; j++)  // j 는 열 (도착 정점)
-                if (a[i][k] + a[k][j] < a[i][j])  // 점화식 distance[i,j] = min(distance[i,j], distance[i,n] + distance[n,j])
-                    a[i][j] =min( a[i][k], a[k][j]);
-*/
-
 int n;
-int tree[2000][2000];
-int temp[2000][2000];
-vector<int>final[2000];
+int tree[2000][2000]; // 변형된 코드 받기 (왼쪽 위만 주었으니 그걸로 오른쪽 아래 채우기)
+int temp[2000][2000]; // 변형된 코드를 통해서 연결되면 1 아니면 0으로 담기
+vector<int>final[2000]; // 리스트 완성
 
 int main() {
     ios::sync_with_stdio(false);
@@ -38,6 +20,7 @@ int main() {
         }
     }
     int flag = 0;
+    // 만약 k를 거쳐서 값이 만들어지지 않는다면 다이렉트로 연결된 상태 -> temp에 1로 넣는다
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
             flag = 0;
@@ -53,7 +36,7 @@ int main() {
             else temp[i][j] = 1;
         }
     }
-
+    // temp에 1로 되어있으면 연결된 것으로 final에 넣어준다
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
             if (temp[i][j] == 1)
