1b2f86033e4d7ed9bb3eb02ee277b70cbcb398e1
선민영 <109517191+stellaMY@users.noreply.github.com>
2023-05-02 22:14:50 +0900

    [Gold II] Title: 지하철, Time: 284 ms, Memory: 6080 KB -BaekjoonHub

--- 백준/Gold/17940. 지하철/지하철.cc
+++ 백준/Gold/17940. 지하철/지하철.cc
@@ -31,6 +31,7 @@ void gogo(int start) {
 	while (!q.empty()) {
 		node now = q.top();
 		q.pop();
+		// 도착지라면 
 		if (now.now == m) {
 			if (result_change < now.change)continue;
 			if (result_cost > now.cost) {
@@ -40,38 +41,29 @@ void gogo(int start) {
 			continue;
 		}
 		for (int i = 0; i < n; i++) {
+			// 연결이 안되어 있다면 패스
 			if (train[now.now][i] == 0)continue;
+			node tmp = now;
+			// 만약 현재 열차와 다음 열차의 소유주가 다르다면 갈아탄 횟수 추가
 			if (own_train[i] != now.train) {
-				if (visited[i].cnt == 0 && visited[i].cost == 0) {
-					visited[i].cnt = now.change + 1;
-					visited[i].cost = now.cost + train[now.now][i];
-					q.push({ i, now.change + 1, now.cost + train[now.now][i], own_train[i] });
-				}
-				else {
-					if (visited[i].cnt < now.change + 1)continue;
-					else if (visited[i].cnt == now.change + 1) {
-						if (visited[i].cost <= now.cost + train[now.now][i])continue;
-					}
-					visited[i].cnt = now.change + 1;
-					visited[i].cost = now.cost + train[now.now][i];
-					q.push({ i, now.change + 1, now.cost + train[now.now][i], own_train[i] });
-				}
+				tmp.change++;
 			}
+			// 방문한적이 없는 역이라면 저장
+			if (visited[i].cnt == 0 && visited[i].cost == 0) {
+				visited[i].cnt = tmp.change;
+				visited[i].cost = tmp.cost + train[tmp.now][i];
+				q.push({ i, tmp.change, tmp.cost + train[tmp.now][i], own_train[i] });
+			}
+			// 방문한적이 있는 역이라면 갈아탄 횟수 확인후 
+			// 더 많이 갈아 탔다면 패스, 같은 횟수라도 더 많은 비용이면 패스
 			else {
-				if (visited[i].cnt == 0 && visited[i].cost == 0) {
-					visited[i].cnt = now.change;
-					visited[i].cost = now.cost + train[now.now][i];
-					q.push({ i, now.change, now.cost + train[now.now][i], own_train[i] });
+				if (visited[i].cnt < tmp.change)continue;
+				else if (visited[i].cnt == tmp.change) {
+					if (visited[i].cost <= tmp.cost + train[tmp.now][i])continue;
 				}
-				else {
-					if (visited[i].cnt < now.change)continue;
-					else if (visited[i].cnt == now.change) {
-						if (visited[i].cost <= now.cost + train[now.now][i])continue;
-					}
-					visited[i].cnt = now.change;
-					visited[i].cost = now.cost + train[now.now][i];
-					q.push({ i, now.change, now.cost + train[now.now][i], own_train[i] });
-				}
+				visited[i].cnt = tmp.change;
+				visited[i].cost = tmp.cost + train[tmp.now][i];
+				q.push({ i, tmp.change,tmp.cost + train[tmp.now][i], own_train[i] });
 			}
 		}
 	}
@@ -79,9 +71,11 @@ void gogo(int start) {
 
 int main() {
 	cin >> n >> m;
+	// 지하철 주인들을 저장
 	for (int i = 0; i < n; i++) {
 		cin >> own_train[i];
 	}
+	// 서로 연결되어 있는 지하철을 저장
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < n; j++) {
 			cin >> train[i][j];
