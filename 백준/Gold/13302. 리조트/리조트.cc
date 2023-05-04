7e3193b1b18acaa42543530fc89af6eb7c02b33d
선민영 <109517191+stellaMY@users.noreply.github.com>
2023-05-04 09:26:38 +0900

    [Gold IV] Title: 리조트, Time: 0 ms, Memory: 2060 KB -BaekjoonHub

--- 백준/Gold/13302. 리조트/리조트.cc
+++ 백준/Gold/13302. 리조트/리조트.cc
@@ -1,59 +1,42 @@
 #include <iostream>
-#include <algorithm>
-#include <vector>
-#include <map>
-
-#define INF 999999999
-
+#include <cmath>
 using namespace std;
 
-int dp[101][202];
+int dp[102][102];
+int n, m;
+int nday[102];
 
-map<int, int> npos_day;
-int ans;
+int buy(int day, int coupon) {
+	if (day > n) return 0;
+	else if (dp[day][coupon] != 2134567890)return dp[day][coupon];
+	else if (nday[day] == 1)return buy(day + 1, coupon);
+	else {
+		int now = dp[day][coupon];
+		now = min(now, buy(day + 1, coupon) + 10000);
+		now = min(now, buy(day + 3, coupon+1) + 25000);
+		now = min(now, buy(day + 5, coupon + 2) + 37000);
+		if (coupon >= 3)now = min(now, buy(day + 1, coupon - 3));
+		dp[day][coupon] = now;
+		return dp[day][coupon];
+	}
+}
 
-int main()
-{
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-    cout.tie(0);
-
-    int m;
-    int n;
-    cin >> n >> m;
-
-    fill(dp[0], dp[101], INF);
-
-    for (int i = 0; i < m; i++)
-    {
-        int j;
-        cin >> j;
-        npos_day[j] = 1;
-    }
-
-    int ans = INF;
-
-    for (int i = 0; i < n + 1; i++) {
-        if (i == 0) dp[i][0] = 0;
-
-        else {
-            for (int j = 0; j < i; j++) {
-                if (npos_day[i] == 1) dp[i][j] = min(dp[i - 1][j], dp[i][j]);
-                else {
-                    dp[i][j] = min(dp[i - 1][j] + 10000, dp[i][j]);
-                    if (j + 3 < n) dp[i][j] = min(dp[i - 1][j + 3], dp[i][j]);
-                    if (i - 3 >= 0 && j - 1>= 0) dp[i][j] = min(dp[i - 3][j - 1] + 25000, dp[i][j]);
-                    if (i - 5 >= 0 && j - 2 >= 0) dp[i][j] = min(dp[i - 5][j - 2] + 37000, dp[i][j]);
-                }
-                if (i == n) {
-                    ans = min(ans, dp[i][j]);
-                    continue;
-                }
-            }
-        }
-    }
-
-
-    cout << ans;
-    return 0;
+int main() {
+	cin >> n>> m;
+	for (int i = 0; i < m; i++) {
+		int num;
+		cin >> num;
+		nday[num] = 1;
+	}
+	for (int i = 0; i <= 101; i++) {
+		for (int j = 0; j <= 101; j++) {
+			dp[i][j] = 2134567890;
+		}
+	}
+	buy(1, 0);
+	int answer = 2134567890;
+	for (int i = 0; i < n; i++) {
+		answer = min(answer, dp[1][i]);
+	}
+	cout << answer;
 }
