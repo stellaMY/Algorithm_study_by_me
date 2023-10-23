2670ed122afdfc52957becbd38d611f25858f673
선민영 <109517191+stellaMY@users.noreply.github.com>
2023-10-23 15:21:11 +0900

    Update 줄 세우기.cc

--- 백준/Gold/2252. 줄 세우기/줄 세우기.cc
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.cc
@@ -4,7 +4,9 @@
 using namespace std;
 
 int n, m;
+// 앞사람이 다 나갔는지 확인
 int flag[33000];
+// 누가 내 뒤에 있는지 확인
 vector<int>line[33000];
 
 int main() {
@@ -15,14 +17,18 @@ int main() {
 		line[f].push_back(b);
 		flag[b]++;
 	}
+	// bfs위한 큐
 	queue<int>q;
+	// 결과를 담는 큐
 	queue<int>final;
+	// 앞사람이 없으면 앞에 서도 된다 
 	for (int i = 0; i < n; i++) {
 		if (flag[i + 1] == 0) {
 			q.push(i + 1);
 			final.push(i + 1);
 		}
 	}
+	// 만약 앞사람이 한명 남으면 남은 앞 한명은 now로 바로 줄세워도 된다 
 	while (!q.empty()) {
 		int now = q.front();
 		q.pop();
